76d6f9ecde294a056e99a8ff0149887253403667
julioharaujo <juliohenrique56@gmail.com>
2025-01-16 19:30:06 -0300

    Commit Inicial - Spring Initializr

---
+++ EstoqueSolidario/src/main/java/br/com/estoquesolidario/EstoqueSolidario/EstoqueSolidarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.estoquesolidario.EstoqueSolidario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueSolidarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueSolidarioApplication.class, args);
+	}
+
+}
---
+++ EstoqueSolidario/src/test/java/br/com/estoquesolidario/EstoqueSolidario/EstoqueSolidarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estoquesolidario.EstoqueSolidario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueSolidarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
